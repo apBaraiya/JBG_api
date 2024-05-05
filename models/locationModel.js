b6a17748e185d5e4ba0556d99d851fdb1e7ead55
mr.baraiya <akshaybaraiya5656@gmail.com>
2024-05-05 14:49:08 +0530

    add new schema

--- models/locationModel.js
+++ models/locationModel.js
@@ -1,54 +1,62 @@
 // locationModel.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const locationSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true
+  id: {
+    type: Number,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  navbtn: {
+    map: {
+      type: String,
+      required: true,
     },
-    img: {
-        type: String,
-        required: true
+    Nbtn: {
+      type: String,
+      required: true,
     },
-    title: {
-        type: String,
-        required: true
+  },
+  contactbtn: {
+    isbtnTwo: {
+      type: Boolean,
+      required: true,
     },
-    desc: {
-        type: String,
-        required: true
+    FirstNumber: {
+      type: Number,
+      required: true,
     },
-    navbtn: {
-        map: {
-            type: String,
-            required: true
-        },
-        Nbtn: {
-            type: String,
-            required: true
-        }
+    SecondNumber: {
+      type: Number,
+      required: function () {
+        return this.contactbtn.isbtnTwo; // Required if isbtnTwo is true
+      },
     },
-    contactbtn: {
-        isbtnTwo: {
-            type: Boolean,
-            required: true
-        },
-        FirstNumber: {
-            type: Number,
-            required: true
-        },
-        SecondNumber: {
-            type: Number,
-            required: function () {
-                return this.contactbtn.isbtnTwo; // Required if isbtnTwo is true
-            }
-        },
-        Cbtn: {
-            type: String,
-            required: true
-        }
-    }
+    Cbtn: {
+      type: String,
+      required: true,
+    },
+  },
+  gola_img:{
+    type: String,
+    required: true,
+  },
+  gola_name: {
+    type: String,
+    required: true,
+  },
 });
 
-const Location = mongoose.model('Location', locationSchema);
+const Location = mongoose.model("Location", locationSchema);
 module.exports = Location;
